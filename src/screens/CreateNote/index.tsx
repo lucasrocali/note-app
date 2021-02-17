91f9bf465b83a0e8fc5f43fa856a33c36a197262
Lucas Rocali <rocali@outlook.com>
2021-02-17 18:50:30 -0300

    feat: CreateNote UI

--- src/screens/CreateNote/index.tsx
+++ src/screens/CreateNote/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList, Styled } from '../../utils/types'
 import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+
+type CreateNoteScreenNavigationProp = StackNavigationProp<
+    RootStackParamList,
+    'CreateNote'
+>;
+
+type CreateNoteScreenRouteProp = RouteProp<RootStackParamList, 'CreateNote'>;
+
+type CreateNoteProps = {
+    route: CreateNoteScreenRouteProp;
+    navigation: CreateNoteScreenNavigationProp;
+};
+
 
 const Container = styled.SafeAreaView`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
+	flex: 1;
+    background-color: ${(props: Styled) => props.theme.color.bgPrimary};
 `;
 
-const Text = styled.Text``;
+const Header = styled.TouchableOpacity`
+    padding-horizontal: ${(props: Styled) => props.theme.spacing.small};     
+`;
 
-export default function CreateNote() {
+const HeaderText = styled.Text`
+    color: ${(props: Styled) => props.theme.color.primary};
+    font-size: 18px;
+    font-weight: 600;
+`
+
+const TextInput = styled.TextInput`
+    flex: 1;
+    margin-vertical: ${(props: Styled) => props.theme.spacing.small};
+	margin-horizontal: ${(props: Styled) => props.theme.spacing.small};
+`;
+
+export default function CreateNote(props: CreateNoteProps) {
+
+    const [text, setText] = useState<string>('')
+
+    const { navigation } = props
+
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <Header onPress={() => navigation.goBack()}>
+                    <HeaderText>{'Back'}</HeaderText>
+                </Header>
+            ),
+            headerRight: () => (
+                <Header onPress={() => console.log('Done')}>
+                    <HeaderText>{'Done'}</HeaderText>
+                </Header>
+            ),
+        });
+    }, [navigation]);
+
+
     return (
         <Container>
-            <Text>CreateNote</Text>
+            <TextInput
+                multiline={true}
+                onChangeText={(text) => setText(text)}
+                value={text}
+                placeholder={'Write here...'}
+                autoCapitalize="none"
+            />
         </Container>
     );
 }
